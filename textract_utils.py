14be91316a4371bf603195976b5465d12b72a377
yagrover <167867212+yagrover@users.noreply.github.com>
2025-07-25 12:48:08 +0530

    Update textract_utils.py

--- textract_utils.py
+++ textract_utils.py
@@ -2,19 +2,22 @@ import boto3
 import time
 import io
 
-def upload_to_s3(file, bucket_name, key):
-    s3 = boto3.client("s3", region_name="us-east-1")
-    file_content = file.read()
-    s3.upload_fileobj(io.BytesIO(file_content), bucket_name, key)
-    time.sleep(2)  # Let S3 sync before Textract
-    return f"s3://{bucket_name}/{key}"
-
-def extract_text_from_textract(bucket, key):
-    textract = boto3.client("textract", region_name="us-east-1")
-
-    response = textract.start_document_text_detection(
-        DocumentLocation={"S3Object": {"Bucket": bucket, "Name": key}}
+def upload_to_s3(file, bucket_name, key, aws_creds):
+    s3 = boto3.client(
+        "s3",
+        region_name="us-east-1",
+        aws_access_key_id=aws_creds["aws_access_key_id"],
+        aws_secret_access_key=aws_creds["aws_secret_access_key"]
     )
+
+def extract_text_from_textract(bucket, key, aws_creds):
+    textract = boto3.client(
+        "textract",
+        region_name="us-east-1",
+        aws_access_key_id=aws_creds["aws_access_key_id"],
+        aws_secret_access_key=aws_creds["aws_secret_access_key"]
+    )
+
     job_id = response["JobId"]
 
     # Wait for job to complete
